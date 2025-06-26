a294152331f3c6accc889e0138a4221fcc674f7d
Hidayatul Syamimi <hdytlsyamimi@gmail.com>
2025-06-26 23:06:11 +0800

    Fix Railway deploy settings

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def role_select():
     return render_template('roleSelect.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
